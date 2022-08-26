47e91edf3ee7a6c22964482b676e21dc064f554a
euarcandius <gfmpenna@gmail.com>
2022-08-26 03:32:19 -0300

    Mod5

---
+++ Source/Helpers/Monster.h
@@ -0,0 +1,14 @@
+//
+// Created by 55229 on 26/08/2022.
+//
+
+#ifndef UNTITLED1_MONSTER_H
+#define UNTITLED1_MONSTER_H
+
+
+class Monster {
+
+};
+
+
+#endif //UNTITLED1_MONSTER_H
---
+++ Source/Monster.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 55229 on 26/08/2022.
+//
+
+#include "Monster.h"
